4f24e4263bfb6d7a322c17a051946681bbd8a013
Gabrielly <gabriellynogueira785@gmail.com>
2026-02-08 10:36:46 -0300

    Primeiro commit do meu portifolio

---
+++ comandos/comandos.js
@@ -0,0 +1,65 @@
+export const front = [
+    {
+        id: 1,
+        imagem: 'https://img.icons8.com/?size=100&id=20909&format=png&color=000000',
+        titulo: 'HTML5'
+    },
+    {
+        id: 2,
+        imagem: 'https://img.icons8.com/?size=100&id=21278&format=png&color=000000',
+        titulo: 'CSS'
+    },
+    {
+        id: 3,
+        imagem: 'https://img.icons8.com/?size=100&id=108784&format=png&color=000000',
+        titulo: 'JavaScript'
+    },
+    {
+        id: 4,
+        imagem: 'https://img.icons8.com/?size=100&id=NfbyHexzVEDk&format=png&color=000000',
+        titulo: 'React'
+    },
+    {
+        id: 5,
+        imagem: 'https://img.icons8.com/?size=100&id=EzPCiQUqWWEa&format=png&color=000000',
+        titulo: 'Bootstrap'
+    },
+    {
+        id: 6,
+        imagem: 'https://img.icons8.com/?size=100&id=HKNzD81eiiSc&format=png&color=000000',
+        titulo: 'JQuery'
+    },
+    {
+        id: 7,
+        imagem: 'https://img.icons8.com/?size=100&id=q8l2X20yd72l&format=png&color=000000',
+        titulo: 'Typescript'
+    },
+    {
+        id: 8,
+        imagem: 'https://img.icons8.com/?size=100&id=zfHRZ6i1Wg0U&format=png&color=000000',
+        titulo: 'Figma'
+    }
+]
+
+export const outros = [
+    {
+        id: 1,
+        imagem: 'https://img.icons8.com/?size=100&id=FQlr_bFSqEdG&format=png&color=000000',
+        titulo: 'Node.js'
+    },
+     {
+        id: 2,
+        imagem: 'https://img.icons8.com/?size=100&id=13679&format=png&color=000000',
+        titulo: 'Java'
+    },
+     {
+        id: 3,
+        imagem: 'https://img.icons8.com/?size=100&id=UGYn5TapNioV&format=png&color=000000',
+        titulo: 'PHP'
+    },
+    {
+        id: 4,
+        imagem: 'https://img.icons8.com/?size=100&id=UFXRpPFebwa2&format=png&color=000000',
+        titulo: 'MySQL'
+    }
+]
